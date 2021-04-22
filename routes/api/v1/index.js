d4b4375f22fb5d0770b73977050abf8ab686ca5a
nickbevers <nick.bevers9@gmail.com>
2021-04-22 21:40:39 +0200

    feat: add 4 functions for posting new transactions, getting transactions and getting the leaderboard with a controller

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const controller = require("../../../controllers/api/v1/transaction");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// Make new transaction
+router.post("/transfers", controller.newTransaction)
+
+//Get all transactions from user
+router.get("/transfers", controller.getTransactions)
+
+//Get all details from specifc transaction
+router.get("/transfers/:id", controller.getTransferById)
+
+//Get #coins per user -> make leaderboard out of it
+router.get("/leaderboard", controller.getLeaderboard)
+
 
 module.exports = router;
