a3d4b32abd7ddd364828e3e18c91f1dbb72bd412
nickbevers <nick.bevers9@gmail.com>
2021-04-23 19:36:42 +0200

    fix: add new transactionRouter and setup for the router in app.js file

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/api/v1/index');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const transactionRouter = require('./routes/api/v1/transaction')
 
 var app = express();
 const cors = require("cors");
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', transactionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
