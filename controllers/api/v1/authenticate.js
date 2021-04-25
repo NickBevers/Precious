6742be0d737263533c3767c0684253c0c17c420d
SarahOorts <r0657122@student.thomasmore.be>
2021-04-25 10:53:38 +0200

    feat: create new folder for passport & add passport.js file to passport folder

--- controllers/api/v1/authenticate.js
+++ controllers/api/v1/authenticate.js
@@ -1,4 +1,4 @@
-const User = require("../../../models/Users");
+const User = require("../../../models/Users"); 
 // const passport = require("passport");
 
 // // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
