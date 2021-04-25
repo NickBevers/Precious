01dd9a97d29a77d1f78b68be1c86bc135c836450
SarahOorts <r0657122@student.thomasmore.be>
2021-04-25 13:21:53 +0200

    fix: move post signup function from users.js routes/api/v1 to users.js from routes

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const usersController = require("../../../controllers/api/v1/users");
-const authenticateController = require("../../../controllers/api/v1/authenticate");
 
 router.get("/", usersController.getAll);
 
 router.get("/login", usersController.login);
 
 router.get("/signup", usersController.signup);
-
-router.post("/signup", authenticateController.postsignup);
 
 module.exports = router;
