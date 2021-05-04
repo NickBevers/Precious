2756f7710d70049f2070491879a4f3d11096a29e
SarahOorts <r0657122@student.thomasmore.be>
2021-05-04 11:06:45 +0200

    fix: added emailrestriction so email should be a student.thomasmore.be email adress, added checkbox check before signup and pasword comparison to confirm password before signup

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -3,27 +3,42 @@ let signup = document.querySelector(".submitBtn").addEventListener("click", func
     let lastname = document.querySelector(".lastname").value;
     let email = document.querySelector(".email").value;
     let password = document.querySelector(".password").value;
-    
-    fetch("http://localhost:3000/users/signup", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            "firstname": firstname,
-            "lastname": lastname,
-            "email": email,
-            "password": password
-        })
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        if(json.status === "success"){
-            console.log("Signup complete!");
+    let confirmpassword = document.querySelector(".confirmpassword").value;
 
-            let token = json.data.token;
-            localStorage.setItem("token", token);
-            window.location.href = "register.html"; // deze locatie waarschijnlijk nog aanpassen
-        }
-    })
+    let emailrestriction = email.indexOf("@student.thomasmore.be");
+    let checkbox = document.querySelector(".custom-checkbox__input");
+
+    if(emailrestriction === -1){
+        console.log("Incorrect email");
+    }
+    else if(checkbox.checked === false){
+        console.log("check the checkbox");
+    }
+    else if(password !== confirmpassword){
+        console.log("password confirm is incorrect");
+    }
+    else{
+        fetch("http://localhost:3000/users/signup", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email,
+                "password": password
+            })
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if(json.status === "success"){
+                console.log("Signup complete!");
+
+                let token = json.data.token;
+                localStorage.setItem("token", token);
+                window.location.href = "register.html"; // deze locatie waarschijnlijk nog aanpassen
+            }
+        })
+    }
 });
