c2a3129e0a7abd9a0f0d8d0cf4fc744e67356d83
nickbevers <nick.bevers9@gmail.com>
2021-05-06 09:39:11 +0200

    Feat: Add date and sender field to a transaction schema

--- models/Transactions.js
+++ models/Transactions.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const transactionSchema = new Schema({
     recipient: String,
+    sender: String,
     amount: Number,
     reason: String,
-    message: String
+    message: String,
+    date: Date
 })
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
