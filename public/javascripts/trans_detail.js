73870740e45a83b861ac917daf82ad57d15f9571
nickbevers <nick.bevers9@gmail.com>
2021-05-12 21:30:03 +0200

    Feat: Add fetch request for transaction by ObjectId + logic to process the promise after the async function

--- public/javascripts/trans_detail.js
+++ public/javascripts/trans_detail.js
@@ -1,22 +1,45 @@
-let data = JSON.parse(localStorage.getItem("transaction"));
+let data = JSON.parse(localStorage.getItem("transactionID"));
+localStorage.removeItem("transactionID")
 console.log(data);
 let from_to = document.querySelector(".recipient");
 let amount = document.querySelector(".amount");
 let reason = document.querySelector(".reason");
 let message = document.querySelector(".message");
 
-if (data.recipient == data.user){
-    from_to.innerHTML = data.sender
-}
-else{
-    from_to.innerHTML = data.recipient
-}
+window.addEventListener("load", function(){
+    let tokencheck = localStorage.getItem("token");
+    if (!tokencheck) {
+        alert("wrong page");
+        window.location.replace("login.html");
+    }
+    else{
+        fetch(`http://localhost:3000/api/v1/transfers/id=${data}`, {
+        method:"get",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${tokencheck}`
+        }
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if(json.status === "succes"){
+                console.log(json.data);
+                console.log(json.user);
+                user_email = json.user.email;
 
-amount.innerHTML = data.amount;
-reason.innerHTML = data.reason;
-message.innerHTML = data.message;
+                if (json.data.recipient == data.user){
+                    from_to.innerHTML = json.data.sender
+                }
+                else{
+                    from_to.innerHTML = json.data.recipient
+                }
 
-localStorage.removeItem("transaction");
-localStorage.removeItem("user");
+                amount.innerHTML = json.data.amount;
+                reason.innerHTML = json.data.reason;
+                message.innerHTML = json.data.message;
+            }
+        })
+    }
+});
 
 
