bc45888ec146afd5971b0b69998b57fad53e0ec4
nickbevers <nick.bevers9@gmail.com>
2021-05-12 18:51:25 +0200

    Feat: Add clicklistener for all the list items to link to the correct detail page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,3 +1,4 @@
+let transactionData, user_email;
 window.addEventListener("load", function(){
     let tokencheck = localStorage.getItem("token");
     if(!tokencheck){
@@ -14,44 +15,42 @@ window.addEventListener("load", function(){
             "Content-Type": "application/json",
             'Authorization': `Bearer ${tokencheck}`
         }
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        if(json.status === "succes"){
-            console.log(json.data);
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if(json.status === "succes"){
+                console.log(json.data);
+                transactionData = json.data;
+                user_email = json.user.email;
 
-            json.data.forEach(element => {
-                // GET DATE FROM DB
-                // let goodDate = new Date(`${element.date}`);
-                // let goodMonth = goodDate.getMonth; 
+                json.data.forEach(element => {
+                    // set clicklistener with transfer to detail view with element data
 
-                if(element.recipient == json.user.email){
-                    let sender = splitEmail(element.sender);
-                    
-                    let transaction = `<li class="list__item">
-                        <p class="list__item--amount">+${element.amount}</p>
-                        <p class="list__item--from-to">${sender[0] + " " + sender[1]}</p>
-                        <i class="fas fa-envelope list__item--message"></i>
-                    </li>
-                    <hr class="list__hr">`
-                    document.querySelector(".list").innerHTML += transaction;
-                }
-                else{
-                    let recipient = splitEmail(element.recipient);
+                    if(element.recipient == json.user.email){
+                        let sender = splitEmail(element.sender);
+                        
+                        let transaction = `<li class="list__item">
+                            <p class="list__item--amount">+${element.amount}P</p>
+                            <p class="list__item--from-to">${sender[0] + " " + sender[1]}</p>
+                            <i class="fas fa-envelope list__item--message"></i>
+                        </li>
+                        <hr class="list__hr">`
+                        document.querySelector(".list").innerHTML += transaction;
+                    }
+                    else{
+                        let recipient = splitEmail(element.recipient);
 
-                    let transaction = `<li class="list__item">
-                        <p class="list__item--amount--sent">-${element.amount}</p>
-                        <p class="list__item--from-to">${recipient[0] + " " + recipient[1]}</p>
-                        <i class="fas fa-envelope list__item--message"></i>
-                    </li>
-                    <hr class="list__hr">`
-                    document.querySelector(".list").innerHTML += transaction;
-                }
-
-                
-            });
-        }
-    })
+                        let transaction = `<li class="list__item">
+                            <p class="list__item--amount--sent">-${element.amount}P</p>
+                            <p class="list__item--from-to">${recipient[0] + " " + recipient[1]}</p>
+                            <i class="fas fa-envelope list__item--message"></i>
+                        </li>
+                        <hr class="list__hr">`
+                        document.querySelector(".list").innerHTML += transaction;
+                    }
+                });
+            }
+        })
     }
 });
 
@@ -64,6 +63,19 @@ function splitEmail(mail){
     return name
 }
 
+let list = document.querySelector(".list");
+list.addEventListener("click", (e) => {
+    let index = [... list.childNodes].indexOf(e.target.parentElement) /3 -1;
+    if(index < 0){index = null}
+    if(index != null){
+        // console.log(index);
+        // console.log(e.target.parentElement);
+        // console.log(transactionData[index]);
+        localStorage.setItem("transaction", JSON.stringify(transactionData[index]));
+        localStorage.setItem("user", user_email);
+        window.location.replace("trans_detail.html");
+    }
+});
 
 
 // fetch("http://localhost:3000/api/v1/", {
