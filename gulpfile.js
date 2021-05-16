6cf171c3136a6f02e9c044a9be491de058eaef52
nickbevers <nick.bevers9@gmail.com>
2021-05-16 20:48:10 +0200

    Feat: Add gulp dependencies for purgecss, changed and cached; Add gulp-purgecss for cleaning up unused css; Add gulp-changed to check which files have been changed; Add gulp-cached for caching files in memory for faster load times

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 // const {src, dest} = require("gulp");
 const sass = require("gulp-sass");
 const {watch, src, dest} = require("gulp");
+const cache = require('gulp-cached');
+const purgecss = require('gulp-purgecss');
+const changed = require('gulp-changed');
 
 // function go(done){
 //     console.log("Here we go!");
@@ -17,11 +20,38 @@ function sass2css(done){
     done();
 }
 
+function cached(done){
+    return src("./public/javascripts/*.js")
+    .pipe(cache('linting'))
+    .pipe(jshint())
+    .pipe(jshint.reporter())
+    done()
+}
+
+function purge(done){
+    return src('./public/stylesheets/*.css')
+    .pipe(purgecss({content: ['./public/*.html']}))
+    .pipe(dest('./public/cleaned_css'))
+    done()
+}
+
+function checkChangedFiles(done){
+    return src("./public/javascripts/*.js")
+    .pipe(changed("./public/javascripts"))
+    .pipe(dest("./public/javascripts"))
+    done()
+}
+
 exports.default = function(done){
     watch("./sass/**/*.scss", sass2css);
+    watch("./public/javascripts/*.js", cached);
+    watch("./public/javascripts/*.js", checkChangedFiles);
     done();
 }
 
 exports.test = test;
 exports.sass2css = sass2css;
+exports.cached = cached;
+exports.purge = purge;
+exports.checkChangedFiles = checkChangedFiles;
 
