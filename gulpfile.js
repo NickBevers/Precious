dd84a4121611b028e0f1a6075c24c05bcf572f0d
SarahOorts <r0657122@student.thomasmore.be>
2021-05-16 13:59:51 +0200

    fix: update gulpfile.js with gulp-sass compiling code and continuous watch option from sass to css in gulp. This is compiled in a new folder named dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
-function go(done){
-    console.log("Here we go!");
+// const {src, dest} = require("gulp");
+const sass = require("gulp-sass");
+const {watch, src, dest} = require("gulp");
+
+// function go(done){
+//     console.log("Here we go!");
+//     done();
+// }
+
+function test(done){
+    console.log("we're done");
     done();
 }
 
-exports.default = go;
+function sass2css(done){
+    return src("./sass/**/*.scss").pipe(sass().on("error", sass.logError)).pipe(dest("./dist"));
+    done();
+}
+
+exports.default = function(done){
+    watch("./sass/**/*.scss", sass2css);
+    done();
+}
+
+exports.test = test;
+exports.sass2css = sass2css;
+
