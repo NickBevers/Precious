daa61b068bbbfbd46c90041f85a27d27eb1f4a5f
nickbevers <nick.bevers9@gmail.com>
2021-05-18 21:45:47 +0200

    Feat: Add fetch request to get the user, and return the user on success; get the user coins from that response and check if the user has enough coins to fullfill the transaction

--- public/javascripts/newTransaction.js
+++ public/javascripts/newTransaction.js
@@ -1,41 +1,65 @@
+let userCoins;
 window.addEventListener("load", function(){
     let tokencheck = localStorage.getItem("token");
+
     if(!tokencheck){
         alert("wrong page");
         window.location.replace("login.html");
     }
     else{
-        document.querySelector(".button").addEventListener("click", () => {
+        document.querySelector(".button").addEventListener("click", async () => {
             let recipient = document.querySelector(".recipient").value;
             let amount = document.querySelector(".amount").value;
             let reason = document.querySelector(".custom-dropdown").value;
             let message = document.querySelector(".message").value;
             let tokencheck = localStorage.getItem("token");
 
-            // check whether user has enough coins
             // Get #coins from user
-            // check if amount < #coins
+            // Get user from db
+            
 
-            fetch("http://localhost:3000/api/v1/transfers", {
-                method: "post",
+            await fetch("http://localhost:3000/users/getuserdata", {
+                method: "get",
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${tokencheck}`
-                },
-                body: JSON.stringify({
-                    "recipient": recipient,
-                    "amount": amount,
-                    "reason": reason,
-                    "message": message
-                })
+                }
             }).then(response => {
                 return response.json();
             }).then(json => {
-                if(json.status === "Succes"){
-                    console.log("SUCCES - Transaction sent")
+                if (json.status == "success"){
+                    console.log(json);
+                    userCoins = json.data[0].coins;
                 }
-            })
+            });
 
+            // check if amount <= #coins
+
+
+            if (userCoins >= amount){
+                fetch("http://localhost:3000/api/v1/transfers", {
+                    method: "post",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${tokencheck}`
+                    },
+                    body: JSON.stringify({
+                        "recipient": recipient,
+                        "amount": amount,
+                        "reason": reason,
+                        "message": message
+                    })
+                }).then(response => {
+                    return response.json();
+                }).then(json => {
+                    if(json.status === "Succes"){
+                        console.log("SUCCES - Transaction sent")
+                    }
+                })
+            }
+            else{
+                console.log("TOOO FAST")
+            }
         });
 
     }
