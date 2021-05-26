93bd9c044e086daea400cd47f48e20ee36503d59
nickbevers <nick.bevers9@gmail.com>
2021-05-26 08:40:06 +0200

    Fix: Remove 2nd port listen to make sure the server works

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function(){
-  console.log("start op 3000");
-});
+// app.listen(3000, function(){
+//   console.log("start op 3000");
+// });
 
 module.exports = app;
